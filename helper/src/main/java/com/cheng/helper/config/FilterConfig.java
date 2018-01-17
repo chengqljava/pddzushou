3c1c293844bf3ca15756361e52d294322fb6b7e8
chengqianliang <1308706231@qq.com>
2018-01-17 15:28:06 +0800

    添加跨域过滤器

--- helper/src/main/java/com/cheng/helper/config/FilterConfig.java
+++ helper/src/main/java/com/cheng/helper/config/FilterConfig.java
@@ -1,23 +1,39 @@
 package com.cheng.helper.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.cheng.helper.filter.CorsFilter;
 import com.cheng.helper.filter.LoginFilter;
+
 @Configuration
 public class FilterConfig {
-	   @Bean
-	    public FilterRegistrationBean memberFilterRegistration() {
-	        FilterRegistrationBean registration = new FilterRegistrationBean();
-	        //注入过滤器
-	        registration.setFilter(new LoginFilter());
-	        //拦截规则
-	        registration.addUrlPatterns("/*");
-	        //过滤器名称
-	        registration.setName("loginFilter");
-	        //过滤器排序
-	        //registration.setOrder(4);
-	        return registration;
-	    }
+    @Bean
+    public FilterRegistrationBean corsFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CorsFilter httpBearerFilter = new CorsFilter();
+        registrationBean.setFilter(httpBearerFilter);
+        List<String> urlPatterns = new ArrayList<String>();
+        urlPatterns.add("/*");
+        registrationBean.setUrlPatterns(urlPatterns);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean memberFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        //注入过滤器
+        registration.setFilter(new LoginFilter());
+        //拦截规则
+        registration.addUrlPatterns("/*");
+        //过滤器名称
+        registration.setName("loginFilter");
+        //过滤器排序
+        //registration.setOrder(4);
+        return registration;
+    }
 }
